e7889a24e23937486ec5a76cba7ec6b86af33b82
Maddie Revill <maddierevill@gmail.com>
2017-01-16 09:22:03 -0800

    Add uniqueness validation on Like model

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,5 +1,6 @@
 class Like < ApplicationRecord
   validates :likeable_id, :likeable_type, :user_id, presence: true
+  validates_uniqueness_of :user_id, scope: [:likeable_type, :likeable_id]
   belongs_to :liker, class_name: :User, foreign_key: :user_id
   belongs_to :likeable, polymorphic: true
 end
