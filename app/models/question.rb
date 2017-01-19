2a1c31b03f4b4b896fd7ee1bef059bbc48f252a9
Maddie Revill <maddierevill@gmail.com>
2017-01-19 14:29:54 -0800

    Remove description from search results

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,10 +1,10 @@
 class Question < ApplicationRecord
   validates :title, presence: true
   belongs_to :author, class_name: "User", foreign_key: :user_id
-  has_many :answers
+  has_many :answers, dependent: :destroy
 
   include PgSearch
   pg_search_scope :search_for,
-    against: %i(title description),
+    against: %i(title),
     using: {tsearch: { highlight: { start_sel: '<strong>', stop_sel: '</strong>'}, prefix: true } }
 end
