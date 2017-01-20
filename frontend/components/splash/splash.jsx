b595d81db657372e522fd49b7b1fb704e29eb94d
Maddie Revill <maddierevill@gmail.com>
2017-01-19 17:07:36 -0800

    Reposition splash image

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -3,7 +3,6 @@ import SessionFormContainer from '../session_form/session_form_container';
 
 const SplashPage = (props) => (
   <div className='splash-page'>
-    <img src="http://res.cloudinary.com/askagram/image/upload/v1484777046/splash2.jpg"/>
     <h1>Answer with pictures.</h1>
     <SessionFormContainer />
   </div>
